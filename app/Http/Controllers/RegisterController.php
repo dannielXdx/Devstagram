9c4ef288f41040e7d29c73a00d7f19717da14093
Daniel Valverde <dannielxdx@outlook.com>
2024-01-20 21:37:40 -0600

    corrección en controlador y devcontainer

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -52,6 +52,6 @@ class RegisterController extends Controller
         auth()->attempt($request->only('email', 'password'));
 
         //Redireccionar
-        return redirect()->route('posts.index');
+        return redirect()->route('posts.index', auth()->user()->username);
     }
 }
